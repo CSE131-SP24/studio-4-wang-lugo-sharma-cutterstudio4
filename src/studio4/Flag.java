2e3ea327ee399e50c7b8c9143b330a2fe57212be
ash3 <ash3@localhost>
2024-02-22 12:20:24 -0600

    commit flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,28 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenRadius(0.05);
+		Color green = new Color(32, 92, 28);
+
+		StdDraw.setPenColor(green);
+		StdDraw.filledCircle(0.5, 0.5, 0.5);
+		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(StdDraw.PINK);
+		StdDraw.filledCircle(0.5, 0.5, 0.4);
+		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledCircle(0.3, 0.6, 0.01);
+		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledCircle(0.7, 0.6, 0.01);
+		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledRectangle(0.5, 0.35, 0.15, 0.01);
+		
+	
 	}
 }
